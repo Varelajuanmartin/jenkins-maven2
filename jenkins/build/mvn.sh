c99c3437d007611ceb94031b870944e5917d94c0
Juan <varelajuanmartin@gmail.com>
2023-03-16 04:06:13 +0000

    Agregada clave dockerhub

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,7 +5,9 @@ echo "** Building jar ***********"
 echo "***************************"
 
 #WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
-WORKSPACE=/home/ubuntu/JenkinsCICD/pipeline/pipeline
+#WORKSPACE=/home/ubuntu/JenkinsCICD/pipeline/pipeline
+WORKSPACE=/home/ubuntu/JenkinsCICD/jenkins_home/workspace/pipeline
+
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 echo ocker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
